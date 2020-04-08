a8e3f09f781c917f96f761fb481237dce887be67
ctdupuis <ctdupuis7@gmail.com>
2020-04-07 20:54:06 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array) {
+  let result = array.find(obj => obj.result === "W")
+  if (!!result) {
+    return result.year
+  } else {
+    return undefined
+  }
+}
